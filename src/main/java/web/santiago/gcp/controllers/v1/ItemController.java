825877dd4034c77d79bd42c399237b9bbae019f2
Thiago Santiago <thiago.vps20@gmail.com>
2019-06-08 17:24:03 -0300

    Add query params nas buscas

--- src/main/java/web/santiago/gcp/controllers/v1/ItemController.java
+++ src/main/java/web/santiago/gcp/controllers/v1/ItemController.java
@@ -8,15 +8,15 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import web.santiago.gcp.entities.Item;
-import web.santiago.gcp.services.ItemService;
+import org.springframework.web.bind.annotation.*;
+import web.santiago.gcp.entities.*;
+import web.santiago.gcp.enuns.TipoColecao;
+import web.santiago.gcp.services.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("v1/item")
@@ -29,6 +29,16 @@ public class ItemController {
      */
     @Autowired
     private ItemService itemService;
+    @Autowired
+    private DlcService dlcService;
+    @Autowired
+    private DvdCdService dvdCdService;
+    @Autowired
+    private HqService hqService;
+    @Autowired
+    private JogoDigitalService jogoDigitalService;
+    @Autowired
+    private JogoTabuleiroService jogoTabuleiroService;
 
     /**
      * Busca todas as Item na base de dados
@@ -37,11 +47,93 @@ public class ItemController {
      * @return ResponseEntity<?>
      */
     @GetMapping
-    public ResponseEntity<?> listAll(Pageable pageable) {
-        Page<Item> items = this.itemService.listAll(pageable);
-        items.getContent().forEach(item -> item.setSaga(null));
+    public ResponseEntity<?> listAll(Pageable pageable,
+                                     // item
+    @RequestParam(value = "titulo", required = false) String titulo,
+    @RequestParam(value = "tipo", required = false) String tipo,
+    @RequestParam(value = "estado", required = false) String estado,
+    @RequestParam(value = "emprestados", required = false) boolean emprestados,
+    @RequestParam(value = "repetidos", required = false) boolean repetidos,
+
+     // dlc
+    @RequestParam(value = "localizacao", required = false) String localizacao,
+
+     //dvdcd
+    @RequestParam(value = "assistidos", required = false) boolean assistidos,
+
+     //hq
+    @RequestParam(value = "editora", required = false) String editora,
+    @RequestParam(value = "universo", required = false) String universo,
+
+     // jogodigital
+    @RequestParam(value = "console", required = false) String console,
+
+     // jogotabuleiro
+    @RequestParam(value = "marca", required = false) String marca) {
+
+        List<Item> items;
+
+        if (tipo != null && tipo != "" && tipo.equals(TipoColecao.DLC.getValor())) {
+
+            logger.info("Get all 'Dlc' from data source");
+            List<Dlc> dlcs = this.dlcService.getAllByLocalizacao(localizacao);
+
+            List<Long> ids = new ArrayList<>();
+            dlcs.forEach(dlc -> ids.add(dlc.getId()));
+            items = this.itemService.getAllByTituloAndTipoAndEstadoAndEmprestadoAndIds(titulo, tipo, estado, emprestados, ids);
+
+        } else if (tipo != null && tipo != "" && tipo.equals(TipoColecao.DVDCD.getValor())) {
+
+            logger.info("Get all 'DvdCd' from data source");
+            List<DvdCd> dvdCds = this.dvdCdService.getAllByAssistidos(assistidos);
+
+            List<Long> ids = new ArrayList<>();
+            dvdCds.forEach(dvdcd -> ids.add(dvdcd.getId()));
+            items = this.itemService.getAllByTituloAndTipoAndEstadoAndEmprestadoAndIds(titulo, tipo, estado, emprestados, ids);
+
+        } else if (tipo != null && tipo != "" && tipo.equals(TipoColecao.HQ.getValor())) {
+
+            logger.info("Get all 'Hq' from data source");
+            List<Hq> hqs = this.hqService.getAllByEditoraAndUniverso(editora, universo);
+
+            List<Long> ids = new ArrayList<>();
+            hqs.forEach(hq -> ids.add(hq.getId()));
+            items = this.itemService.getAllByTituloAndTipoAndEstadoAndEmprestadoAndIds(titulo, tipo, estado, emprestados, ids);
+
+        } else if (tipo != null && tipo != "" && tipo.equals(TipoColecao.JOGODIGITAL.getValor())) {
+
+            logger.info("Get all 'JogoDigital' from data source");
+            List<JogoDigital> jogoDigitals = this.jogoDigitalService.getAllByConsole(console);
+
+            List<Long> ids = new ArrayList<>();
+            jogoDigitals.forEach(jogoDigital -> ids.add(jogoDigital.getId()));
+            items = this.itemService.getAllByTituloAndTipoAndEstadoAndEmprestadoAndIds(titulo, tipo, estado, emprestados, ids);
+
+        } else if (tipo != null && tipo != "" && tipo.equals(TipoColecao.JOGOTABULEIRO.getValor())) {
+
+            logger.info("Get all 'JogoTabuleiro' from data source");
+            List<JogoTabuleiro> jogoTabuleiros = this.jogoTabuleiroService.getAllByMarca(marca);
+
+            List<Long> ids = new ArrayList<>();
+            jogoTabuleiros.forEach(jogoTabuleiro -> ids.add(jogoTabuleiro.getId()));
+            items = this.itemService.getAllByTituloAndTipoAndEstadoAndEmprestadoAndIds(titulo, tipo, estado, emprestados, ids);
+
+        } else {
+
+            logger.info("Get all 'Item' from data source");
+            items = this.itemService.getAll();
+        }
+
+        if (repetidos) {
+            items = items.stream().filter(e -> e.getQuantidade() > 1).collect(Collectors.toList());
+        }
+
+
+
+        items.forEach(item -> item.setSaga(null));
+        Page<Item> pages = new PageImpl<>(items, pageable, items.size());
         logger.info("Get all 'Item' from data source");
-        return new ResponseEntity<>(items, HttpStatus.OK);
+        return new ResponseEntity<>(pages, HttpStatus.OK);
     }
 
     /**
