ad4afcb84635097c531a662c251835b79c89aea5
Thiago Santiago <thiago.vps20@gmail.com>
2019-06-08 11:44:20 -0300

    Correção de erro nos cors

--- src/main/java/web/santiago/gcp/configurations/SecurityConfig.java
+++ src/main/java/web/santiago/gcp/configurations/SecurityConfig.java
@@ -34,9 +34,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable().authorizeRequests()
+        http.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
-                .antMatchers(HttpMethod.GET, SEARCH_URL).permitAll()
                 .antMatchers("/**").hasRole("USER")
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
--- src/main/java/web/santiago/gcp/configurations/SecurityConstants.java
+++ src/main/java/web/santiago/gcp/configurations/SecurityConstants.java
@@ -5,7 +5,7 @@ public class SecurityConstants {
     public static final String SECRET = "gcpWeb";
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
-    public static final String SIGN_UP_URL = "/*/users/**";
+    public static final String SIGN_UP_URL = "/*/user/**";
     public static final String SEARCH_URL = "livreAcesso";
-    public static final long EXPIRATION_TIME = 86400000L; //24 horas
+    public static final long EXPIRATION_TIME = 86400000L; // 24 horas
 }
