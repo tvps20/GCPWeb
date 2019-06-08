7c702beec59c94889ef164e6d0c1c276752869e6
Thiago Santiago <thiago.vps20@gmail.com>
2019-06-08 14:36:23 -0300

    configuração de cors

--- src/main/java/web/santiago/gcp/configurations/SecurityConfig.java
+++ src/main/java/web/santiago/gcp/configurations/SecurityConfig.java
@@ -9,7 +9,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import web.santiago.gcp.services.CustumUserDetailService;
+
+import java.util.Arrays;
 
 import static web.santiago.gcp.configurations.SecurityConstants.SEARCH_URL;
 import static web.santiago.gcp.configurations.SecurityConstants.SIGN_UP_URL;
@@ -34,8 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, SIGN_UP_URL).permitAll()
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(SIGN_UP_URL).permitAll()
                 .antMatchers("/**").hasRole("USER")
                 .and()
                 .addFilter(new JWTAuthenticationFilter(authenticationManager()))
@@ -58,5 +63,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
